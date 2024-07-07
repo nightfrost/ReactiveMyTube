5fe164952eba07ca2a1081ad87dd784e15855981
nightfrost <lucas.rosenorn@outlook.dk>
2024-07-07 15:18:48 +0200

    RMT-MODULE Added consumer module.

---
+++ ReactiveMyTube-Consumer/src/main/java/io/nightfrost/reactivemytubeconsumer/ReactiveMyTubeConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.nightfrost.reactivemytubeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMyTubeConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMyTubeConsumerApplication.class, args);
+    }
+
+}
