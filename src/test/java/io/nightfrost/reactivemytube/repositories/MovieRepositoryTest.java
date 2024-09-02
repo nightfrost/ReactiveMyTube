2ae89ee094778d25cabc3707b132a79bbdba9773
nightfrost <lucas.rosenorn@outlook.dk>
2024-09-02 09:37:57 +0200

    New Repository tests and general cleanup

---
+++ src/test/java/io/nightfrost/reactivemytube/repositories/MovieRepositoryTest.java
@@ -0,0 +1,68 @@
+package io.nightfrost.reactivemytube.repositories;
+
+import io.nightfrost.reactivemytube.models.Metadata;
+import io.nightfrost.reactivemytube.models.Tags;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+public class MovieRepositoryTest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    private String movieId;
+
+    @BeforeEach
+    public void initialize() throws IOException {
+        // Load the MP4 file from classpath
+        ClassPathResource resource = new ClassPathResource("movies/20131214_NEVER GIVE UP YOUR WAAAAAAAAAAAAY.mp4");
+        byte[] content = Files.readAllBytes(resource.getFile().toPath());
+
+        // Mock FilePart
+        FilePart filePart = mock(FilePart.class);
+        when(filePart.filename()).thenReturn("20131214_NEVER GIVE UP YOUR WAAAAAAAAAAAAY.mp4");
+        when(filePart.content()).thenReturn(Flux.just(DefaultDataBufferFactory.sharedInstance.wrap(content)));
+
+        //Mock Metadata
+        Metadata metadata = mock(Metadata.class);
+        when(metadata.getName()).thenReturn("Never Give up your way");
+        when(metadata.getPosterUrl()).thenReturn("https://google.dk/img.jpg");
+        ArrayList<Tags> tags = new ArrayList<>();
+        tags.add(Tags.COMEDY);
+        tags.add(Tags.HORROR);
+        when(metadata.getTags()).thenReturn(tags);
+
+        // Call the repository method to store the file
+        Mono<ObjectId> result = movieRepository.store(Mono.just(filePart), metadata);
+
+        // Verify the result
+        StepVerifier.create(result)
+                .expectNextMatches(entity -> {
+                    movieRepository.getResource(entity.toString()).flatMapMany(resource -> resource.getContent().flatMap(tempcontent -> tempcontent.));
+                    // Add assertions to check if the entity is correctly stored
+                    // For example:
+                    return entity.get().equals("test-video.mp4")
+                            && entity.getContent().equals(content);
+                })
+                .verifyComplete();
+    }
+}
--- src/test/java/io/nightfrost/reactivemytube/repositories/UserRepositoryTest.java
+++ src/test/java/io/nightfrost/reactivemytube/repositories/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package io.nightfrost.reactivemytube.repositories;
 
 import io.nightfrost.reactivemytube.models.User;
+import io.nightfrost.reactivemytube.services.HelperService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.reactivestreams.Publisher;
@@ -21,6 +23,35 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private String userId;
+
+    private String username;
+
+    @BeforeEach
+    public void initialize() {
+        final String TEST_USERNAME = "usernameTest";
+        User user = User.builder().firstname("FirstName")
+                .lastname("LastName")
+                .username(TEST_USERNAME)
+                .password("password")
+                .email("email@email.dk")
+                .phone("29282726")
+                .dob(LocalDate.now())
+                .nationality("DK")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .enabled(true)
+                .build();
+
+        User test_user = userRepository.deleteAll().thenMany(userRepository.save(user)).blockFirst();
+        assert test_user != null;
+        assert test_user.getId() != null && !test_user.getId().isBlank();
+        assert test_user.getUsername() != null && !test_user.getUsername().isBlank();
+        userId = test_user.getId();
+        username = test_user.getUsername();
+    }
+
+    //CREATE
     @Test
     public void GivenValidUserData_WhenSave_ThenSuccess() {
         User user = User.builder().firstname("FirstName")
@@ -42,13 +73,49 @@ public class UserRepositoryTest {
                 .verifyComplete();
     }
 
+    //GET by id
     @Test
     public void GivenCorrectUserId_WhenGetById_ThenSuccess() {
-        final String TEST_USERNAME = "usernameTest";
-        User user = User.builder().firstname("FirstName")
-                .lastname("LastName")
-                .username(TEST_USERNAME)
-                .password("password")
+        Publisher<User> setup = userRepository.findById(userId);
+
+        Publisher<User> composite = Mono
+                .from(setup);
+
+        StepVerifier
+                .create(composite)
+                .consumeNextWith(account -> {
+                    assert account.getId() != null;
+                    assert account.getUsername().equals(username);
+                    assert account.isEnabled();
+                    assert account.getEmail().equals("email@email.dk");
+                }).verifyComplete();
+    }
+
+    //GET by username
+    @Test
+    public void GivenCorrectUsername_WhenGetByUsername_ReturnSuccess() {
+        Publisher<User> setup = userRepository.findByUsername(username);
+
+        Publisher<User> composite = Mono
+                .from(setup);
+
+        StepVerifier
+                .create(composite)
+                .consumeNextWith(account -> {
+                    assert account.getId().equalsIgnoreCase(userId);
+                    assert account.getUsername().equals(username);
+                    assert account.isEnabled();
+                    assert account.getEmail().equals("email@email.dk");
+                }).verifyComplete();
+    }
+
+    //UPDATE
+    @Test
+    public void GivenCorrectUserData_WhenUpdateUser_ReturnSuccess() {
+        User updatedUser = User.builder().firstname("UpdatedFirstName")
+                .lastname("UpdatedLastName")
+                .username("updatedUsername")
+                .password("Updatedpassword")
                 .email("email@email.dk")
                 .phone("29282726")
                 .dob(LocalDate.now())
@@ -58,21 +125,29 @@ public class UserRepositoryTest {
                 .enabled(true)
                 .build();
 
-        Publisher<User> setup = userRepository.deleteAll().then(userRepository.save(user));
-        Mono<User> find = userRepository.findByUsername(TEST_USERNAME);
+        Publisher<User> setup = userRepository.findById(userId)
+                .flatMap(user -> userRepository.save((User) HelperService.partialUpdate(user, updatedUser)))
+                .thenMany(userRepository.findById(userId));
 
-        Publisher<User> composite = Mono
-                .from(setup)
-                .then(find);
+        Publisher<User> composite = Mono.from(setup);
 
-        StepVerifier
-                .create(composite)
-                .consumeNextWith(account -> {
-                    assert account.getId() != null;
-                    assert account.getUsername().equals(TEST_USERNAME);
-                    assert account.isEnabled();
-                    assert account.getEmail().equals("email@email.dk");
-                }).verifyComplete();
+        StepVerifier.create(composite).consumeNextWith(user -> {
+            assert user.getUsername().equals(updatedUser.getUsername());
+            assert user.getFirstname().equals(updatedUser.getFirstname());
+            assert user.getLastname().equals(updatedUser.getLastname());
+            assert user.getPassword().equals(updatedUser.getPassword());
+        }).verifyComplete();
     }
 
+    //DELETE
+    @Test
+    public void GivenCorrectUserId_WhenDeleteUser_ReturnSuccess() {
+        Publisher<User> setup = userRepository.findById(userId)
+                .flatMap(user -> userRepository.delete(user))
+                .thenMany(userRepository.findById(userId));
+
+        Publisher<User> composite = Mono.from(setup);
+
+        StepVerifier.create(composite).expectNextCount(0).verifyComplete();
+    }
 }
